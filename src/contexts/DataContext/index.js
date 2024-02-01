e4d6c3363671d3e0fd3035ac844d6198c94d1f98
Nicolas Leclaire <nicolasleclaire5721@gmail.com>
2024-02-01 10:38:17 +0100

    Initial commit

---
+++ src/contexts/DataContext/index.js
@@ -0,0 +1,59 @@
+import PropTypes from "prop-types";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+const DataContext = createContext({});
+
+export const api = {
+  loadData: async () => {
+    const json = await fetch("/events.json");
+    return json.json();
+  },
+};
+
+export const DataProvider = ({ children }) => {
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const [last, setLast] = useState(null);
+  const getData = useCallback(async () => {
+    try {
+      setData(await api.loadData());
+    } catch (err) {
+      setError(err);
+    }
+  }, []);
+  useEffect(() => {
+    if (data) {
+    // Tri pour l'affichage des évènements par rapport à leur date
+    setLast(data.events.sort((evtA, evtB) =>
+    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1)[0])
+    return;}
+    getData();
+  });
+  
+  return (
+    <DataContext.Provider
+      // eslint-disable-next-line react/jsx-no-constructed-context-values
+      value={{
+        data,
+        error,
+        last
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+DataProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export const useData = () => useContext(DataContext);
+
+export default DataContext;
